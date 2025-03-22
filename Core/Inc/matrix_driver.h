942006154cd09648d1c91d76569948585979e68c
aalexxc0888 <achitsaz@purdue.edu>
2025-03-22 02:53:32 -0400

    DMA is now working to automatically draw screen on LED matrix display

--- Core/Inc/matrix_driver.h
+++ Core/Inc/matrix_driver.h
@@ -3,25 +3,31 @@
 
 #include "stm32f4xx_hal.h"
 
-#define ROW 16
+// 32 x 64 LED matrix
+#define ROW 32
 #define COL 64
-#define MEMSIZE ROW * COL
+
+// screen data structure only needs half the rows, but needs twice the cols to allow room for clk pin to change
+#define SCREEN_ROW ROW / 2
+#define SCREEN_COL COL * 2
 
 // structure for DMA output (16 bits/ 2B)
 typedef struct
 {
-	unsigned int dead1: 2;
+	unsigned int dead:  2;
 	unsigned int rgb1:  3;
 	unsigned int rgb2:  3;
 	unsigned int addr:  5;
-	unsigned int dead2: 3;
+	unsigned int oe:    1;
+	unsigned int lat:   1;
+	unsigned int clk:   1;
 } hub75_gpio_t;
 
 
 void initMatrix(void);
-void setup_TIM8(void);
-void setup_DMA(void);
-void fillScreen(uint8_t);
-void initScreenAddr();
+void setup_TIM8(uint8_t, uint8_t);
+void setup_DMA2_S1(void);
+void fillScreen(uint8_t, uint8_t);
+void initScreen(void);
 
 #endif
--- Core/Src/matrix_driver.c
+++ Core/Src/matrix_driver.c
@@ -3,144 +3,100 @@
 #include <string.h>
 
 
-// structure to contain data of what screen is displaying
-hub75_gpio_t screen [ROW][COL];
-volatile uint16_t temp = 0;
-volatile uint8_t curr_row = 0;
-volatile uint8_t curr_col = 0;
+// structure to contain frame data ready to be directly DMA'ed to matrix pins
+hub75_gpio_t screen [SCREEN_ROW][SCREEN_COL];
 
 void initMatrix(void)
 {
-	// ensure all RGB/Addr pins are 0 by default
+	// ensure all matrix GPIO  pins are 0 by default
 	HAL_GPIO_WritePin(GPIOE, (GPIO_PIN_All & ~(GPIO_PIN_0 | GPIO_PIN_1)), GPIO_PIN_RESET);
-	// ensure clk, latch, and OE pins are 0 by default
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3 | GPIO_PIN_4, GPIO_PIN_RESET); // latch and oe
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET); // clk
 
 	// clear structure of any lingering data
-	memset(screen, 0, sizeof(screen));
-	initScreenAddr();
-	fillScreen(1); // fill purple
+	initScreen();
+	fillScreen(3, 6); // purple and teal
 
 
-	setup_DMA();
-	setup_TIM8();
-	DMA2_Stream1 -> CR |= DMA_SxCR_EN;
+	setup_DMA2_S1();
+	setup_TIM8(1, 2);
 }
 
-void fillScreen(uint8_t color)
+void fillScreen(uint8_t color1, uint8_t color2)
 {
-	for(int row = 0; row < ROW; row++)
+	for(int row = 0; row < SCREEN_ROW; row++)
 	{
-		for(int col = 0; col < COL; col++)
+		for(int col = 0; col < SCREEN_COL; col++)
 		{
-			screen[row][col].rgb1 = color;
-			screen[row][col].rgb2 = color;
+			screen[row][col].rgb1 = color1;
+			screen[row][col].rgb2 = color2;
 		}
 	}
 }
 
-void initScreenAddr()
+void initScreen()
 {
-	for(uint8_t row = 0; row < ROW; row++)
+	for(int row = 0; row < SCREEN_ROW; row++)
 	{
-		for(uint8_t col = 0; col < COL; col++)
+		for(int col = 0; col < SCREEN_COL; col++)
 		{
+			screen[row][col].dead = 0;
+			screen[row][col].rgb1 = 0;
+			screen[row][col].rgb2 = 0;
 			screen[row][col].addr = row;
+
+			// setup clk to automatically rise and fall over each DMA transaction
+			if(col % 2)
+			{
+				screen[row][col].clk = 0;
+			}
+			else
+			{
+				screen[row][col].clk = 1;
+			}
+
+			// once the final pixel is shifted in, latch it and raise oe
+			if(col >= (SCREEN_COL - 2))
+			{
+				screen[row][col].lat = 1;
+				screen[row][col].oe  = 1;
+			}
+			else
+			{
+				screen[row][col].lat = 0;
+				screen[row][col].oe  = 0;
+			}
 		}
 	}
 }
 
-void setup_TIM8(void)
+void setup_TIM8(uint8_t psc, uint8_t arr)
 {
-	  // Turn on the clock for timer 8
-	  RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
-	  // set the clk freq
-	  TIM8->PSC = 100-1;
-	  TIM8->ARR = 200-1;
-	  // Set for Upcounting
-	  TIM8->CR1 &= ~TIM_CR1_DIR;
-	  // Set the Channel 1 for PWM mode 2
-	  TIM8->CCMR1 |= (TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2| TIM_CCMR1_OC1M_0);
-	  // set the CCR value to produce PWM of desired duty cycle
-	  TIM8->CCR1 = 100; // 50% duty cycle
-	  // Enable DMA transfer at the @posedge of the clk
-	  TIM8->DIER |= TIM_DIER_CC1IE;
-	  TIM8->DIER |= TIM_DIER_UDE;
-	  TIM8->CCER |= TIM_CCER_CC1E;
-	  TIM8->BDTR |= TIM_BDTR_MOE;
-	  HAL_NVIC_EnableIRQ(TIM8_CC_IRQn);
-	  // Turn on timer
-	  TIM8->CR1 |= 0x1;
+	// Turn on the clock for timer 8
+	RCC  -> APB2ENR |= RCC_APB2ENR_TIM8EN; // TIM8 clock
+	TIM8 -> CR1     &= ~TIM_CR1_DIR; // count up
+
+	TIM8 -> PSC   = psc - 1;
+	TIM8 -> ARR   = arr - 1; // f_tim = fclk / (psc * arr) Hz
+	TIM8 -> DIER |= TIM_DIER_UDE; // on timer update, trigger the DMA
+	TIM8 -> CR1  |= TIM_CR1_CEN; // enable the timer
 }
 
-void setup_DMA(void)
+void setup_DMA2_S1(void)
 {
   __HAL_RCC_DMA2_CLK_ENABLE();
 
   DMA2_Stream1 -> CR &= ~DMA_SxCR_EN;
 
-  DMA2_Stream1 -> CR |= (DMA_SxCR_CHSEL_2 | DMA_SxCR_CHSEL_1 | DMA_SxCR_CHSEL_0); // channel 7
-  // Total size of transfer 64 cols
-  DMA2_Stream1 -> NDTR = 1;
-  // Memory Location
-  DMA2_Stream1 -> PAR = (uint32_t) &temp; // source in mem2mem
-  DMA2_Stream1 -> M0AR  = (uint32_t) (&(GPIOE->ODR)); // dest in mem2mem
-  // periph access : 16 bits
-  // mem access : 16 bits
-  DMA2_Stream1 -> CR |= (DMA_SxCR_MSIZE_0);
-  DMA2_Stream1 -> CR |= (DMA_SxCR_PSIZE_0);
-//  DMA2_Stream1 -> CR |= DMA_SxCR_PINC;
-//  DMA2_Stream1 -> CR |= DMA_SxCR_CIRC;
-  DMA2_Stream1 -> CR |= DMA_SxCR_DIR_1; // mem to mem
+  DMA2_Stream1 -> CR  |= (DMA_SxCR_CHSEL_2 | DMA_SxCR_CHSEL_1 | DMA_SxCR_CHSEL_0); // channel 7 - triggering off of TIM8_UP
+  DMA2_Stream1 -> CR  |= (DMA_SxCR_MSIZE_1); // memory size is 32-bit per transaction
+  DMA2_Stream1 -> CR  |= (DMA_SxCR_PSIZE_1); // peripheral size is 32-bit per transaction
+  DMA2_Stream1 -> CR  |= DMA_SxCR_MINC;
+  DMA2_Stream1 -> CR  |= DMA_SxCR_CIRC;
+  DMA2_Stream1 -> CR  |= DMA_SxCR_DIR_0; // memory to peripheral transfer
 
-//  DMA2_Stream2 -> CR |= DMA_SxCR_TCIE;
-//  HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
-//  HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
-}
-
-//void DMA2_Stream2_IRQHandler(void) {
-//
-//  DMA2_Stream2->CR &= ~DMA_SxCR_EN;
-//  // acknowledge pending bit
-//  DMA2 -> LIFCR |= DMA_LIFCR_CTCIF2;
-//  GPIOA -> BSRR |= GPIO_BSRR_BS4; // latch
-////	  GPIOA -> BSRR |= GPIO_BSRR_BS3; // OE
-//
-//  curr_row++;
-//  if(curr_row > 15)
-//  {
-//    curr_row = 0;
-//  }
-//
-//	  DMA2_Stream2->PAR = (uint32_t) (screen + (curr_row*64*2));
-//	  GPIOA -> BSRR |= GPIO_BSRR_BR4; // latch
-////	  GPIOA -> BSRR |= GPIO_BSRR_BR3; // OE
-//  DMA2_Stream2->CR |= DMA_SxCR_EN;
-//
-//}
-
-void TIM8_CC_IRQHandler(void)
-{
-	TIM8->SR &= ~TIM_SR_CC1IF;
-	temp = screen[curr_row][curr_col].dead1 | (screen[curr_row][curr_col].rgb1 << 2) | (screen[curr_row][curr_col].rgb2 << 5) | (screen[curr_row][curr_col].addr << 8 | (screen[curr_row][curr_col].dead2 << 13));
-	DMA2 -> LIFCR |= DMA_LIFCR_CTCIF2;
-	DMA2_Stream1 -> CR |= DMA_SxCR_EN;
-	curr_col++;
-	if(curr_col > 63)
-	{
-		GPIOA -> BSRR |= GPIO_BSRR_BS4; // activate latch
-//		GPIOA -> BSRR |= GPIO_BSRR_BS3; // OE
-		curr_row++;
-		if(curr_row > 15)
-		{
-			curr_row = 0;
-		}
-		curr_col = 0;
-		GPIOA -> BSRR |= GPIO_BSRR_BR4;
-//		GPIOA -> BSRR |= GPIO_BSRR_BR3; // OE
-	}
-
+  DMA2_Stream1 -> NDTR = SCREEN_ROW * SCREEN_COL; // 2048 transfers
+  DMA2_Stream1 -> PAR  = (uint32_t) (&(GPIOE->ODR));
+  DMA2_Stream1 -> M0AR = (uint32_t) screen;
+  DMA2_Stream1 -> CR  |= DMA_SxCR_EN; // Enable DMA
 }
 
 
