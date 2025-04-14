79703a11a2515bcc04e3007f463cd5a676ceee3b
aalexxc0888 <achitsaz@purdue.edu>
2025-04-14 01:29:30 -0400

    Alex: Implemented double buffering DMA peripheral for DAC. Audio is now perfectly smooth with no crackle/cuts. CPU is also no longer blocked by DMA transaction to DAC being finished since a 16-depth circular buffer is used to store each packet of samples. So if the DMA is running behind, the CPU can simply store its latest packet to the circular buffer and work on getting the next one. Implemented a basic circular buffer .c/.h pair to handle checking full, empty, push, and pop conditions.

--- Core/Inc/dac_output.h
+++ Core/Inc/dac_output.h
@@ -3,15 +3,22 @@
 
 #include "stm32f4xx_hal.h"
 #include "shared_consts.h"
+#include "circular_buffer.h"
+
+#define DAC_BUFFER_Q_SIZE 16 // circular buffer can hold 16 sets of samples at a time
+
+typedef struct
+{
+	uint16_t DACBufferQ[DAC_BUFFER_Q_SIZE][SONG_BUFF_SIZE];
+	circ_buff_t buffStatus;
+} dac_buff_q_t;
 
 void initDAC();
 void initDacBuffer();
-void fillDacBuffer();
+void fillDacBuffer(float[SONG_BUFF_SIZE]);
 
 // STM32 Peripheral Settings
 void setup_TIM6(uint16_t, uint16_t);
-
-
-extern volatile int dacDONE;
+void setup_DMA1_S5();
 
 #endif
--- Core/Src/dac_output.c
+++ Core/Src/dac_output.c
@@ -1,8 +1,8 @@
 #include "dac_output.h"
 
-uint16_t DACBuffer[SONG_BUFF_SIZE];
-int halfWord = 0;
-volatile int dacDONE = 1;
+uint16_t DACFrontBuffer[SONG_BUFF_SIZE];
+uint16_t DACBackBuffer[SONG_BUFF_SIZE];
+dac_buff_q_t DACCircBuff;
 
 void initDAC()
 {
@@ -11,44 +11,61 @@ void initDAC()
 	// for DAC channel 1 (PA4)
 	DAC -> CR &= ~DAC_CR_TSEL1; // trigger on TIM6 update event
 	DAC -> CR |= DAC_CR_TEN1;  // only update DAC on a trigger
+	DAC -> CR |= DAC_CR_DMAEN1; // trigger the DMA on TIM6 update event
 	DAC -> CR |= DAC_CR_EN1;   // enable the DAC to start working
 
-	// these params chosen for 256 Hz sampling rate
-	// PSC = 125
-	// ARR = 1000 (but then this is 3x faster for some reason, so I chose 3k, not 1k)
-	// use PSC = 5 and ARR = 145 for 44.1 KHz sampling rate later (maybe multiply ARR by 3 again..?)
-//	setup_TIM6(125, 10);
 	initDacBuffer();
-	setup_TIM6(44, 47); // for sending 44,100 samples every second to DAC
+	setup_TIM6(44, 47); // for sending ~44,100 samples every second to DAC
+	setup_DMA1_S5();
 }
 
 void initDacBuffer()
 {
-	for(int i = 0; i < SONG_BUFF_SIZE; i++)
+	initCircBuff(DAC_BUFFER_Q_SIZE, &(DACCircBuff.buffStatus)); // set up parameters for buffStatus
+	for(int sample = 0; sample < SONG_BUFF_SIZE; sample++)
 	{
-		DACBuffer[i] = 0;
-		DACBuffer[i] = 0;
-	}
-	// DACBuffer now represents 2^16 different voltage values ranging from [0, Vcc]
-}
-
-void fillDacBuffer(int16_t songBuff[SONG_BUFF_SIZE])
-{
-	int16_t min = 0x7fff;
-	for(int i = 0; i < 1024; i++)
-	{
-		if(songBuff[i] < min)
+		DACFrontBuffer[sample] = 0;
+		DACBackBuffer[sample]  = 0;
+		for(int i = 0; i < 16; i++)
 		{
-			min = songBuff[i];
+			DACCircBuff.DACBufferQ[i][sample] = 0;
 		}
 	}
-	min = (min < 0) ? -1 * min : min;
-	for(int i = 0; i < SONG_BUFF_SIZE; i++)
+	// DACBuffers now represents 2^16 different voltage values ranging from [0, Vcc]
+}
+
+void fillDacBuffer(float songBuffer[SONG_BUFF_SIZE])
+{
+	int16_t min = 0x7fff;
+	int16_t signed16Sample; // preserve a signed version of the sample since the DACBuffers are unsigned
+	while(DACCircBuff.buffStatus.full); // Wait for the DMA to consume a set of samples and send to the DAC before creating more
+
+	/* CRITICAL SECTION START:
+	 * Do not allow DMA to interrupt and consume a new sample set while we are writing
+	 */
+	NVIC_DisableIRQ(DMA1_Stream5_IRQn);
+	for(int sample = 0; sample < SONG_BUFF_SIZE; sample++)
 	{
-		DACBuffer[i] = songBuff[i] + min; // offset to +ve only
-		DACBuffer[i] = DACBuffer[i] >> 4; // we only have 12 bit DAC, so chop lowest 4 bits
+		signed16Sample = (songBuffer[sample] / MAX_AMPLITUDE) * (float) MAX_VAL_INT16_T; // convert float back into int16_t
+		DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.wrPtr][sample] = signed16Sample;
+		if(signed16Sample < min)
+		{
+			min = signed16Sample;
+		}
 	}
-	dacDONE = 0;
+	min = (min < 0) ? -1 * min : min; // force found min value to be +ve
+
+	for(int sample = 0; sample < SONG_BUFF_SIZE; sample++)
+	{
+		DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.wrPtr][sample] = (int16_t) DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.wrPtr][sample] + min; // offset to +ve only for converting to unsigned value
+		DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.wrPtr][sample] = DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.wrPtr][sample] >> 4; // we only have 12 bit DAC, so chop lowest 4 bits
+	}
+	pushCircBuff(&(DACCircBuff.buffStatus));
+	NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+	/* CRITICAL SECTION END:
+	 * DMA may consume now
+	 */
+
 }
 
 void setup_TIM6(uint16_t psc, uint16_t arr)
@@ -56,9 +73,8 @@ void setup_TIM6(uint16_t psc, uint16_t arr)
 	// Turn on the clock for timer 6
 	RCC  -> APB1ENR |= RCC_APB1ENR_TIM6EN; // TIM6 clock
 
-	TIM6 -> PSC   = psc - 1;
-	TIM6 -> ARR   = arr - 1; // f_tim = fclk / (psc * arr) Hz
-	TIM6 -> DIER |= TIM_DIER_UIE; // on timer update, call interrupt
+	TIM6 -> PSC  = psc - 1;
+	TIM6 -> ARR  = arr - 1; // f_tim = fclk / (psc * arr) Hz
 	TIM6 -> CR2 |= TIM_CR2_MMS_1; // Setup TIM6 to enable TRGO on update event
 
 	TIM6 -> CR1 |= TIM_CR1_CEN; // enable the timer
@@ -66,17 +82,53 @@ void setup_TIM6(uint16_t psc, uint16_t arr)
 	NVIC_EnableIRQ(TIM6_DAC_IRQn); // enable the update interrupt event for TIM6 and DAC1/DAC2 underrrun error
 }
 
-void TIM6_DAC_IRQHandler()
+void setup_DMA1_S5(void)
 {
-	// acknowledge the pending bit for the interrupt
-	TIM6 -> SR &= ~TIM_SR_UIF;
+  __HAL_RCC_DMA1_CLK_ENABLE();
 
-	DAC -> DHR12L1 = DACBuffer[halfWord]; // fill the DAC's DHR with next 16-bits (lower 4 get chopped off)
-	halfWord++;
+  DMA1_Stream5 -> CR &= ~DMA_SxCR_EN;
 
-	if(halfWord == SONG_BUFF_SIZE) // Finished sending out sample, wait for the next one.
+  DMA1_Stream5 -> CR  |= (DMA_SxCR_CHSEL_2 | DMA_SxCR_CHSEL_1 | DMA_SxCR_CHSEL_0); // channel 7 - triggering off of DAC1
+  DMA1_Stream5 -> CR  |= (DMA_SxCR_MSIZE_0); // memory size is 16-bit per transaction
+  DMA1_Stream5 -> CR  |= (DMA_SxCR_PSIZE_0); // peripheral size is 16-bit per transaction
+  DMA1_Stream5 -> CR  |= DMA_SxCR_MINC;
+  DMA1_Stream5 -> CR  |= DMA_SxCR_CIRC;
+  DMA1_Stream5 -> CR  |= DMA_SxCR_DIR_0; // memory to peripheral transfer
+  DMA1_Stream5 -> CR  |= DMA_SxCR_DBM;   // enable double buffer mode
+  DMA1_Stream5 -> CR  |= DMA_SxCR_TCIE; // Transfer-complete interrupt for double buffer mode
+
+
+  DMA1_Stream5 -> NDTR = SONG_BUFF_SIZE; // number of 16-bit transactions
+  DMA1_Stream5 -> PAR  = (uint32_t) (&(DAC -> DHR12L1));
+  DMA1_Stream5 -> M0AR = (uint32_t) DACFrontBuffer;
+  DMA1_Stream5 -> M1AR = (uint32_t) DACBackBuffer;
+
+  NVIC_EnableIRQ(DMA1_Stream5_IRQn); // enable the IRQ
+  DMA1_Stream5 -> CR  |= DMA_SxCR_EN; // Enable DMA
+}
+
+// Consumer - Pop the latest set of samples from DACBufferQ and transmit to the DAC via DMA
+void DMA1_Stream5_IRQHandler()
+{
+	if(DMA1 -> HISR & DMA_HISR_TCIF5)
 	{
-		halfWord = 0;
-		dacDONE = 1;
+		DMA1 -> HIFCR |= DMA_HIFCR_CTCIF5; // acknowledge
+	}
+
+
+	if(!DACCircBuff.buffStatus.empty) // check that circular buffer is not empty
+	{
+		for(int sample = 0; sample < SONG_BUFF_SIZE; sample++)
+		{
+			if(DMA1_Stream5 -> CR & DMA_SxCR_CT)
+			{
+				DACFrontBuffer[sample] = DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.rdPtr][sample];
+			}
+			else
+			{
+				DACBackBuffer[sample]  = DACCircBuff.DACBufferQ[DACCircBuff.buffStatus.rdPtr][sample];
+			}
+		}
+		popCircBuff(&(DACCircBuff.buffStatus));
 	}
 }
